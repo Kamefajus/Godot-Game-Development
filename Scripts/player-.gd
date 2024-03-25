83b3a99cd7e9b51a6bd1c7683393f57813d3fa63
Th1nkeR1S <airidasstoskus@gmail.com>
2024-03-25 16:47:38 +0200

    Commit from Airidas

--- Scripts/player-.gd
+++ Scripts/player-.gd
@@ -1,7 +1,8 @@
 extends RigidBody2D
 
 var wheels = []
-var speed = 2000
+var speed = 1000
+var max_speed = 50
 
 func _ready():
 	wheels = get_tree().get_nodes_in_group("wheel")
@@ -9,4 +10,11 @@ func _ready():
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
 		for wheel in wheels:
-			wheel.apply_torque_impulse(speed * delta * 60)
+			if wheel.angular_velocity < max_speed:
+				wheel.apply_torque_impulse(speed * delta * 60)
+	
+	if Input.is_action_pressed("ui_left"):
+		for wheel in wheels:
+			if wheel.angular_velocity > -max_speed:
+				wheel.apply_torque_impulse(-speed * delta * 60)
+
