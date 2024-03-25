7b8a19ce9e6f1042175a7bcc43a1917936d0c77d
Eidvilas Markevičius <markeviciuseidvilas@gmail.com>
2024-03-25 22:39:54 +0200

    Add level progress indicator to the hud.

--- Scripts/gui.gd
+++ Scripts/gui.gd
@@ -1,17 +1,23 @@
 extends RichTextLabel
 
+var x_initial = 0
 var time_start = 0
 var time_now = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	x_initial = $"../../Player/Camera2D2".get_screen_center_position().x;
 	time_start = Time.get_unix_time_from_system()
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	time_now = Time.get_unix_time_from_system()
-	var time_elapsed = snapped(time_now - time_start, 0.01)
-	get_node(".").text = "Timer: 	" + str(time_elapsed) + "		seconds"
-	pass
+	var x_current = $"../../Player/Camera2D2".get_screen_center_position().x;
+	var x_final = $"../../Finish".position.x;
+	var progress = (x_current - x_initial) / (x_final - x_initial) * 100;
+	text = "[Progress: " + str(snapped(progress, 0)) + "%] ";
+	
+	var time_now = Time.get_unix_time_from_system()
+	var time_elapsed = time_now - time_start
+	text += "[Time elapsed: " + str(snapped(time_elapsed, 0)) + " seconds] "
+
