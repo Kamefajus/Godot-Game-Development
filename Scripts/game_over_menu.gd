9b96675318e984f369d28a291535d85e6ebe93ab
Th1nkeR1S <airidasstoskus@gmail.com>
2024-04-15 21:46:46 +0300

    fix2

--- Scripts/game_over_menu.gd
+++ Scripts/game_over_menu.gd
@@ -10,13 +10,15 @@ func enable():
 
 func _on_restart_pressed():
 	GlobalVariables.coins += $"../Player".coins
-	get_tree().reload_current_scene()
 	get_tree().paused = false
+	get_tree().reload_current_scene()
+	
 
 func _on_quit_pressed():
 	get_tree().quit()
 	
 func _on_back_to_main_menu_pressed():
 	GlobalVariables.coins += $"../Player".coins
-	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
 	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+	
