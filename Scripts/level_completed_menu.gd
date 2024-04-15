5fef4994d4d0607fe7c356df098cbb3bdcf8844b
Eidvilas Markevičius <markeviciuseidvilas@gmail.com>
2024-04-16 01:01:33 +0300

    Add finish animation.

---
+++ Scripts/level_completed_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+var enabled = false
+
+func enable():
+	position = $"../Player/Camera2D2".get_screen_center_position();
+	show();
+	enabled = true
+	get_tree().paused = true
+
+func _on_restart_pressed():
+	GlobalVariables.coins += $"../Player".coins
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+	
+
+func _on_quit_pressed():
+	get_tree().quit()
+	
+func _on_back_to_main_menu_pressed():
+	GlobalVariables.coins += $"../Player".coins
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
