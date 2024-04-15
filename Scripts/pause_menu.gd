154ed1e6d31fd5036ae8277ffd8cd433a8562eac
Th1nkeR1S <airidasstoskus@gmail.com>
2024-04-15 21:40:43 +0300

    fix 1

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -29,6 +29,7 @@ func _on_sound_pressed():
 
 func _on_back_to_main_menu_pressed():
 	GlobalVariables.coins += $"../Player".coins
-	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
 	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+	
 
