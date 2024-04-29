342dab542b9e1d99ae6efa1949aaf5384c83f48f
Eidvilas Markevičius <markeviciuseidvilas@gmail.com>
2024-04-30 00:50:38 +0300

    Make "next level" button at level 4 go to level 1.

--- Scripts/level_completed_menu.gd
+++ Scripts/level_completed_menu.gd
@@ -12,7 +12,12 @@ func _on_next_level_pressed():
 	GlobalVariables.coins += $"../Player".coins
 	GlobalVariables.save()
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://Scenes/level_" + str(GlobalVariables.current_level + 1) + ".tscn")
+	if (GlobalVariables.current_level < 4):
+		GlobalVariables.current_level += 1
+		get_tree().change_scene_to_file("res://Scenes/level_" + str(GlobalVariables.current_level) + ".tscn")
+	else:
+		GlobalVariables.current_level = 1
+		get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
 
 func _on_restart_pressed():
 	GlobalVariables.coins += $"../Player".coins
