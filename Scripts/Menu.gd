1dcfe12e35d802c48df01d0d9301bc1051a0951f
Th1nkeR1S <161242488+Th1nkeR1S@users.noreply.github.com>
2024-05-07 22:52:05 +0300

    05-07

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -17,9 +17,13 @@ func _on_quit_pressed():
 func _on_upgrades_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Garage.tscn")
 
-
 func _on_transition_animation_finished(anim_name):
 	get_tree().change_scene_to_file("res://Scenes/level_select_screen.tscn")
 
 func _on_music_pressed():
 	AudioServer.set_bus_mute(music_bus, not AudioServer.is_bus_mute(music_bus))
+
+
+func _on_story_pressed():
+	$Select.play()
+	get_tree().change_scene_to_file("res://Scenes/story.tscn")
